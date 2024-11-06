4fbcbcbe518479d27142754d1b90bb0aeb7a8aac
deelaka99 <123wejith@gmail.com>
2024-11-06 09:07:02 +0530

    created constants

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+//color for correct answer
+const Color correct = Color(0xFF32AB58);
+//color for incorrect answer
+const Color incorrect = Color(0xFFAB3232);
+//color neutral
+const Color neutral = Color(0xFFE7E7E7);
+//Background color
+const Color background = Color(0xFF1A3464);
